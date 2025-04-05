872a38539274c3fa27b80574924603f019d6668d
Erik Dubois <118379307+awa03@users.noreply.github.com>
2025-04-04 20:31:49 -0400

    Add Basic Text Generation

--- markov/include/relations.h
+++ markov/include/relations.h
@@ -4,18 +4,59 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <iomanip>
+#include <random>
+#include <stack>
 #include <unordered_set>
 
 
 #include "thirdparty/json.hpp"
 using json = nlohmann::json;
 
+struct Pair {
+    std::string first, second;
+    int freq;
+
+    bool operator==(const Pair& other) const{
+        return first == other.first && second == other.second;
+    }
+
+    Pair& operator=(const Pair& other){
+        if (this != &other) {
+            first = other.first;
+            second = other.second;
+            freq = other.freq;
+        }
+        return *this;
+
+    }
+
+ std::string toString() const {
+        return first + second;
+    }
+};
+
+struct PairHash {
+  size_t operator()(const Pair& p) const {
+        return std::hash<std::string>()(p.first) ^ std::hash<std::string>()(p.second);
+    }
+};
+
 typedef struct {
-    std::unordered_map<char, std::unordered_map<char, int>> table;
-    std::unordered_map<char, int> occurance;
+    std::unordered_map<std::string, std::unordered_map<std::string, int>> table;
+    std::unordered_map<std::string, std::unordered_map<std::string, double>> probability;
+    std::unordered_map<std::string, int> occurance;
+    std::unordered_map<Pair, std::string, PairHash> vocab;
+    std::unordered_map<std::string, Pair> reversed_vocab;
 } RelationTable;
 
 RelationTable* load_table_from_file(const char* file_path);
+void load_vocab(RelationTable* relation, json builder);
+void reverse_vocab(RelationTable* relation);
+Pair getRandomPair(RelationTable* relation);
 
+std::string getHighestProbability(RelationTable* relation, std::string token);
+std::string decodeToken(RelationTable* relation, std::string token);
+std::string generateSentence(RelationTable* relation, int max_length =100);
 
 #endif // RELATIONS_H_
--- markov/src/main.cpp
+++ markov/src/main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 #include "relations.h"
 
+#define FLAG_IMPLEMENTATION
+#include "thirdparty/flag.h"
 
 /**
 *
 *
 */
 
-int main(int argc, char *argv[]){
-    RelationTable* table = load_table_from_file(argv[1]);
+void usage(void) {
+    fprintf(stderr, "Usage: %s [OPTIONS]\n", flag_program_name());
+    fprintf(stderr, "OPTIONS:\n");
+    flag_print_options(stderr);
+}
 
+int main(int argc, char *argv[]){
+    bool *help      = flag_bool("help", false, "Print this help message");
+    char **bpe_path = flag_str("bpe", NULL, "Path to BPE file (MANADATORY)");
+    uint64_t *limit = flag_uint64("limit", 100, "Text size");
+
+    if (!flag_parse(argc, argv)) {
+        usage();
+        flag_print_error(stderr);
+        return 1;
+    }
+
+    if(*bpe_path == nullptr) return -1;
+    RelationTable* table = load_table_from_file(*bpe_path);
+
+    srand(time(0));
+    std::cout << generateSentence(table, *limit);
 }
